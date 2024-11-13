295b6d17e04d1dff87478039f5b3243cbd3726c2
dr-strange1905 <rathoresiddharth1905@gmail.com>
2024-11-13 23:34:05 +0530

    added item delete and some other related functionalities

--- js/script4.js
+++ js/script4.js
@@ -1,6 +1,9 @@
 const addButton = document.querySelector('.btn');
 const itemList = document.querySelector('.items');
 const itemInput = document.querySelector('.form-input');
+const clearBtn = document.getElementById('clear');
+
+const itemFilter = document.getElementById('filter');
 
 // creating function to add new items into the list
 const addItem = (e) => {
@@ -25,6 +28,7 @@ const addItem = (e) => {
   listItem.appendChild(span);
   listItem.appendChild(button);
   itemList.appendChild(listItem);
+  checkUI();
 };
 
 // fucntion to create a new item button
@@ -40,7 +44,49 @@ const createButton = (classes) => {
 const createIcon = (classes) => {
   let icon = document.createElement('i');
   icon.className = classes;
+  return icon;
+};
+
+// function to remove items from the list
+const removeItem = (e) => {
+  // if (e.target.parentElement.tagName === 'BUTTON') {
+  //   e.target.parentElement.parentElement.remove();
+  // }
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+};
+
+// function to clear all of the list items
+const clearItems = () => {
+  while (itemList.firstChild) {
+    itemList.firstChild.remove();
+  }
+  checkUI();
+};
+
+// function to check if the list is empty and if it is then hidding the filter and clear button
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
 };
 
 // adding event listener to the submit button
 addButton.addEventListener('click', addItem);
+// event listener for the list items
+itemList.addEventListener('click', removeItem);
+
+// adding event listener for the clear all button
+
+clearBtn.addEventListener('click', clearItems);
+
+checkUI();
